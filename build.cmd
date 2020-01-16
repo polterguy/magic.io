8d1277adeba6f392e118094400234c9233033b1c
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:25:39 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 cd %~dp0
-dotnet build magic.io.contracts/magic.io.contracts.csproj --configuration Release
-dotnet build magic.io.services/magic.io.services.csproj --configuration Release
-dotnet build magic.io.controller/magic.io.controller.csproj --configuration Release
+dotnet build magic.io.contracts/magic.io.contracts.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet build magic.io.services/magic.io.services.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet build magic.io.controller/magic.io.controller.csproj --configuration Release --source https://api.nuget.org/v3/index.json
